3f9a3b299402f4b5a87c957d44deef27b50439b5
richnathan <richnathan@gmail.com>
2016-06-16 21:29:05 +0800

    Create isr4.py

---
+++ isr4.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python 
+
+# Python script for opening a flatfile and sending the value through the yun bridge, call with FlowMeterInterrupt.ino
+# made for use with Linkit Smart 7688 Duo
+# Author  Nathan Richardson
+# Date    5/25/16
+# Open source, use as you wish
+
+import sys
+sys.path.insert(0, '/usr/lib/python2.7/bridge/') 
+
+from bridgeclient import BridgeClient as bridgeclient
+
+value = bridgeclient()                              
+
+target = open("/IoT/data/dat.txt", "r")
+c = target.readline()
+value.put("90",(c))	
+target.close()
+
+sys.exit()
